ec7a875012d99337ed2a74bc4aea3be2595a347a
wangui_kara <karawangui@gmail.com>
2025-04-22 09:34:30 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,9 @@
 create database educrack;
 use educrack;
-
+CREATE TABLE lecturers (
+  lec_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) UNIQUE NOT NULL
+);
 
