5e2e37ccdbf871f6c475d4fa41dafd38935bd4a9
Catherine <consolatacatherinenjoki@gmail.com>
2025-04-22 09:37:15 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -6,4 +6,13 @@ CREATE TABLE lecturers (
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) UNIQUE NOT NULL
 );
+CREATE TABLE  students (
+  student_id varchar(4) NOT NULL,
+  fullname varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(255) NOT NULL,
+  course_Id varchar(5) DEFAULT NULL,
+  PRIMARY KEY (student_id),
+  UNIQUE KEY email (email));
+
 
